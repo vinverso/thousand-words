a82f048c4dd1c9b3d66c8845f92efbca7cdcfa4f
Vincent Inverso <vinverso@vincents-mbp.home>
2014-02-17 10:38:52 -0500

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Vincent Inverso on 2/17/14.
+//  Copyright (c) 2014 Vincent Inverso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VIAppDelegate class]));
+    }
+}
